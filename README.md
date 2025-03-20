0bec6382bad934ffdb43d19ea1cccb052edcf6f5
c0smic <swatsonjones@mun.ca>
2025-03-20 20:41:26 -0230

    almost the entire game

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # pysteriods
- pygame-ce Asteriods, a fun little side project
+
+pygame-ce Asteriods, a fun little side project of mine, playing around with pygame and a more interactive experience.
+
+## How to play
+
+You move around with **W** to thrust, and **Q** and **E** to rotate your ship.
+
+## The goal
+
+Your goal is to survive as long as you can before taking 5 hits and losing!
---
+++ game.py
@@ -0,0 +1,53 @@
+import pygame
+from scripts import player
+from scripts import asteroid
+
+# helpful const definitions for later
+TITLE = "py-asteroids"
+WINDOW = (800, 600)
+REFRESH = 60
+
+def main() -> None:
+    pygame.init()
+    display = pygame.display.set_mode(WINDOW)
+    clock = pygame.time.Clock()
+    active = True
+
+    # Player init
+    plr = player.Player()
+
+    # asteroids init
+    asteroids = [asteroid.Asteroid(WINDOW[0], WINDOW[1]) for _ in range(10)]
+
+    dt = 1 / REFRESH
+
+    # Logic / Rendering loop
+    while active:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                active = False
+
+        pkey = pygame.key.get_pressed()  # Get key states before updating player
+        
+        # Update game objects
+        for rock in asteroids:
+            rock.update(dt)
+
+        plr.update(dt, pkey, asteroids)
+
+        # Blanking fill before drawing
+        display.fill("black")
+
+        # Draw game objects
+        for rock in asteroids:
+            rock.draw(display)
+
+        plr.draw(display)
+
+        pygame.display.flip()
+        dt = clock.tick(REFRESH) / 1000 
+
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
